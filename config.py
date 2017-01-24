f0ce477dc89351ff2d7689244a4d9648bbdbf701
yonixw <yonixw@gmail.com>
2017-01-24 23:51:25 +0200

    basic json reading and error handaling

--- config.py
+++ config.py
@@ -1,11 +1,21 @@
+# Server config
+# =====================================================
 SERVER_PORT = 8000
 GUI_SEPERATOR = '\\'
 
-
-
-
+# Strings
+# =====================================================
 HELP_MESSAGE = """
-This site is part of the Python Safe (R-Pi).
-Usage, POST json:
-    { 'trycomplete' : <path> } for autocomplete paths
+<pre>
+This site is part of the Python Safe (R-Pi).\r\n
+Usage, POST json:\r\n
+    \t{ "action" : "trycomplete" , "path" : path } for autocomplete paths\r\n
+    \t{ "action" : "lock" , "path" : path } to lock a file/folder\r\n
+    \t{ "action" : "unlock" , "path" : path } to unlock a file/folder\r\n
+</pre>
 """
+
+# Script config
+# =====================================================
+DETAILED_ERRORS = True # Set false if security is an issue
+STACK_TRACE_LIMIT = 100
--- mainServer.py
+++ mainServer.py
@@ -1,27 +1,52 @@
 import config
 import json
+import traceback
 from BaseHTTPServer import BaseHTTPRequestHandler, HTTPServer
 
 
 class mySafeHandler(BaseHTTPRequestHandler):
-
     def log_message(self, format, *args):
         # No built in logs.
         return
 
     def do_GET(self):
-        # Handler for the GET requests
+        # Send help message for usage.
+
         self.send_response(200)
         self.send_header('Content-type', 'text/html')
         self.end_headers()
         # Send the html message
-        self.wfile.write("Hello World ! your path: " + self.path)
+        self.wfile.write(config.HELP_MESSAGE)
+        return
+
+    def jsonHeaders(self, statuscode):
+        # General start for any response
+        self.send_response(statuscode)
+        self.send_header('Content-type', 'application/json')
+        self.end_headers()
+
+    def do_POST(self):
+        try:
+            bodyContent = self.rfile.read(int(self.headers['Content-Length']))
+            jsonBody = json.loads(bodyContent)
+
+            self.jsonHeaders(200)
+        except Exception as ex:
+            if (config.DETAILED_ERRORS):
+                self.jsonHeaders(500)
+                self.wfile.write(
+                    json.dumps(
+                        {"error": ex.message, "traceback": traceback.format_exc(config.STACK_TRACE_LIMIT)}
+                    )
+                )
+            else:
+                self.send_error(500, "Error handling POST in main module")
         return
 
 
 try:
     server = HTTPServer(("", config.SERVER_PORT), mySafeHandler)
-    print 'Started httpserver on port ', config.SERVER_PORT
+    print 'Started server on port ', config.SERVER_PORT
 
     # Start server
     server.serve_forever()
